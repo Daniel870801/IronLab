56accf24c75b416e612bbad171f631de0e98e7d2
helmi chaaba <98045032+helmichaaba@users.noreply.github.com>
2022-03-07 17:08:25 +0100

    Add files via upload

---
+++ Module1/LAB23/LAB 23 MURDER MYSTERY.sql
@@ -0,0 +1,33 @@
+SELECT description FROM crime_scene_report
+WHERE date = '20180115' AND type = 'murder' AND city = 'SQL City';
+WITH witness1 AS (
+    SELECT id FROM person
+    WHERE address_street_name = 'Northwestern Dr'
+    ORDER BY address_number DESC LIMIT 1
+), witness2 AS (
+    SELECT id FROM person
+    WHERE INSTR(name, 'Annabel') > 0 AND address_street_name = 'Franklin Ave'
+), witnesses AS (
+    SELECT *, 1 AS witness FROM witness1
+    UNION
+    SELECT *, 2 AS witness FROM witness2
+)
+SELECT witness, transcript FROM witnesses
+LEFT JOIN interview ON witnesses.id = interview.person_id;
+
+WITH gym_checkins AS (
+    SELECT person_id, name
+    FROM get_fit_now_member
+    LEFT JOIN get_fit_now_check_in ON get_fit_now_member.id = get_fit_now_check_in.membership_id
+    WHERE membership_status = 'gold' 
+
+      AND check_in_date = '20180109'
+), suspects AS (
+    SELECT gym_checkins.person_id, gym_checkins.name, plate_number, gender
+    FROM gym_checkins
+    LEFT JOIN person ON gym_checkins.person_id = person.id
+    LEFT JOIN drivers_license ON person.license_id = drivers_license.id
+)
+SELECT * FROM suspects
+WHERE INSTR(plate_number, 'H42W') > 0 AND gender = 'male';
+
